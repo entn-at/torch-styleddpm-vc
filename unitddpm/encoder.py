86896eeb864573dde13a6c436817fe64e7e722fb
revsic <revsic99@gmail.com>
2022-07-15 00:33:11 +0900

    encoder: add style encoder

---
+++ unitddpm/encoder.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .resblock import ResidualBlock
+
+
+class StyleEncoder(nn.Module):
+    """Encode the style token from inputs.
+    """
+    def __init__(self,
+                 styles: int,
+                 domains: int,
+                 channels: int,
+                 kernels: int,
+                 stages: int,
+                 blocks: int):
+        """Initializer.
+        Args:
+            styles: size of the style tokens.
+            domains: the number of the output domains.
+            channels: size of the convolutional channels.
+            kernels: size of the convolutional kernels.
+            stages: the number of the residual blocks.
+            blocks: the number of the convolution blocks before residual connection.
+        """
+        super().__init__()
+        self.styles, self.domains = styles, domains
+        self.blocks = nn.ModuleList([
+            ResidualBlock(channels, kernels, blocks)
+            for _ in range(stages)])
+
+        self.neck = ResidualBlock(channels, kernels, blocks)
+
+        self.proj = nn.Sequential(
+            nn.ReLU(),
+            nn.Linear(channels, domains * styles))
+
+    def forward(self, inputs: torch.Tensor, code: torch.Tensor) -> torch.Tensor:
+        """Generate the style code.
+        Args:
+            inputs: [torch.float32; [B, C, T]], input tensor, spectrogram.
+            code: [torch.long; [B]], domain code.
+        """
+        x = inputs
+        for block in self.blocks:
+            # [B, C, T // 2 ** i]
+            x = F.interpolate(block(x), scale_factor=0.5, mode='nearest')
+        # [B, C]
+        x = self.neck(x).mean(dim=-1)
+        # B
+        bsize = x.shape[0]
+        # [B, domains, styles]
+        x = self.proj(x).view(bsize, self.domains, self.styles)
+        # [B, styles]
+        return x[torch.arange(bsize), code]

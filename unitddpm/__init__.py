27415de8af80064191507f5f0973126be3e2e15f
revsic <revsic99@gmail.com>
2022-07-15 00:39:05 +0900

    init: Add domain encoder

--- unitddpm/__init__.py
+++ unitddpm/__init__.py
@@ -1,8 +1,10 @@
+from colorama import Style
 import torch
 import torch.nn as nn
 
 from .config import Config
 from .embedder import Embedder
+from .encoder import StyleEncoder
 from .unet import UNet
 
 
@@ -16,14 +18,18 @@ class UnitDDPMVC(nn.Module):
         """
         super().__init__()
         self.proj_inputs = nn.Sequential(
-            nn.Conv1d(config.mel * 2, config.channels, 1),
+            nn.Conv1d(config.mel * 2, config.channels * 2, 1, groups=2),
             nn.ReLU())
 
         self.embedder = Embedder(
             config.pe, config.embeddings, config.steps, config.mappings)
 
+        self.encoder = StyleEncoder(
+            config.styles, config.domains, config.channels, config.kernels,
+            config.style_stages, config.style_blocks)
+
         self.unet = UNet(
-            config.channels, config.kernels, config.embeddings,
+            config.channels, config.kernels, config.embeddings, config.styles,
             config.stages, config.blocks)
 
         self.proj_outputs = nn.Sequential(
@@ -33,20 +39,24 @@ class UnitDDPMVC(nn.Module):
     def denoise(self,
                 signal: torch.Tensor,
                 outpart: torch.Tensor,
+                code: torch.Tensor,
                 steps: torch.Tensor) -> torch.Tensor:
         """Denoise the signal w.r.t. outpart signal.
         Args:
             signal: [torch.float32; [B, mel, T]], input signal.
             outpart: [torch.float32; [B, mel, T]], counter part signal.
+            code: [torch.long; [B]], domain code.
             steps: [torch.long; [B]], diffusion steps, zero-based.
         Returns:
             [torch.float32; [B, mel, T]], denoised signal.
         """
         # [B, C, T]
-        x = self.proj_inputs(torch.cat([signal, outpart], dim=1))
+        x, y = self.proj_inputs(torch.cat([signal, outpart], dim=1)).chunk(2, dim=1)
+        # [B, styles]
+        style = self.encoder(y, code)
         # [B, E]
         embed = self.embedder(steps)
         # [B, C, T]
-        x = self.unet(x, embed)
+        x = self.unet(x, embed, style)
         # [B, mel, T]
         return self.proj_outputs(x)
--- unitddpm/config.py
+++ unitddpm/config.py
@@ -1,10 +1,11 @@
 class Config:
     """Configuration for UNIT-DDPM.
     """
-    def __init__(self, mel: int):
+    def __init__(self, mel: int, speakers: int):
         """Initializer.
         Args:
             mel: size of the spectrogram features.
+            speakers: the number of the speakers.
         """
         self.mel = mel
 
@@ -18,3 +19,9 @@ class Config:
         self.kernels = 3
         self.stages = 4
         self.blocks = 2
+
+        # style encoder
+        self.styles = 32
+        self.domains = speakers
+        self.style_blocks = 2
+        self.style_kernels = 3

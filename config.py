f1f60439e44d532e291ea5e220662396247b73de
revsic <revsic99@gmail.com>
2022-07-17 17:33:23 +0900

    config: add configurations

---
+++ config.py
@@ -0,0 +1,87 @@
+from disc.config import Config as DiscConfig
+from speechset.config import Config as DataConfig
+from starddpm.config import Config as ModelConfig
+
+
+class TrainConfig:
+    """Configuration for training loop.
+    """
+    def __init__(self, sr: int, hop: int):
+        """Initializer.
+        Args:
+            sr: sample rate.
+            hop: stft hop length.
+        """
+        # optimizer
+        self.learning_rate = 1e-4
+        self.beta1 = 0.9
+        self.beta2 = 0.999
+
+        # loader settings
+        self.batch = 8
+        self.shuffle = True
+        self.num_workers = 4
+        self.pin_memory = True
+
+        # train iters
+        self.epoch = 1000
+
+        # segment length
+        self.seglen = int(sr * 0.5) // hop
+
+        # path config
+        self.log = './log'
+        self.ckpt = './ckpt'
+
+        # model name
+        self.name = 't1'
+
+        # commit hash
+        self.hash = 'unknown'
+
+
+class Config:
+    """Integrated configuration.
+    """
+    def __init__(self, speakers: int = 1):
+        """Initializer.
+        Args:
+            speakers: the number of the speakers.
+        """
+        self.data = DataConfig(batch=None)
+        self.train = TrainConfig(self.data.sr, self.data.hop)
+        self.model = ModelConfig(self.data.mel, speakers)
+        self.disc = DiscConfig(self.model.mel, speakers)
+
+    def update_speakers(self, speakers: int):
+        """Update the number of the speakers.
+        Args:
+            speakers: the number of the speakers.
+        """
+        self.model.domains = speakers
+        self.disc.domains = speakers
+
+    def dump(self):
+        """Dump configurations into serializable dictionary.
+        """
+        return {k: vars(v) for k, v in vars(self).items()}
+
+    @staticmethod
+    def load(dump_):
+        """Load dumped configurations into new configuration.
+        """
+        conf = Config()
+        for k, v in dump_.items():
+            if hasattr(conf, k):
+                obj = getattr(conf, k)
+                load_state(obj, v)
+        return conf
+
+
+def load_state(obj, dump_):
+    """Load dictionary items to attributes.
+    """
+    for k, v in dump_.items():
+        if hasattr(obj, k):
+            setattr(obj, k, v)
+    return obj

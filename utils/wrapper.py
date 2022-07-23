eb2b39462ec49143916c4dce912da25c25392952
revsic <revsic99@gmail.com>
2022-07-23 18:36:30 +0900

    disc: Remove discriminators

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -1,4 +1,3 @@
-import sched
 from typing import Dict, List, Tuple
 
 import numpy as np
@@ -6,7 +5,6 @@ import torch
 import torch.nn.functional as F
 
 from config import Config
-from disc import Discriminator
 from starddpm import StarDDPMVC
 
 
@@ -15,18 +13,15 @@ class TrainingWrapper:
     """
     def __init__(self,
                  model: StarDDPMVC,
-                 disc: Discriminator,
                  config: Config,
                  device: torch.device):
         """Initializer.
         Args:
             model: StarDDPM model.
-            disc: discriminator.
             config: training configurations.
             device: torch device.
         """
         self.model = model
-        self.disc = disc
         self.config = config
         self.device = device
 
@@ -44,14 +39,16 @@ class TrainingWrapper:
         Args:
             bunch: input tensors.
                 ids: [np.long; [B]], auxiliary ids.
+                text: [np.long; [B, S]], text token sequence.
                 pitch: [np.float32; [B, T]], pitch sequence.
                 mel: [np.float32; [B, T, mel]], mel spectrogram.
-                lengths: [np.long; [B]], spectrogram lengths.
+                textlen: [np.long; [B]], text lengths.
+                mellen: [np.long; [B]], spectrogram lengths.
         Returns:
             randomly segmented spectrogram and audios.
         """
         # [B], [B, T], [B, T, mel], [B]
-        ids, pitch, mel, lengths = bunch
+        ids, _, pitch, mel, _, lengths = bunch
         # [B]
         start = np.random.randint(lengths - self.config.train.seglen)
         # [B, S]
@@ -60,7 +57,7 @@ class TrainingWrapper:
         # [B, S, mel]
         mel = np.array(
             [m[s:s + self.config.train.seglen] for m, s in zip(mel, start)])
-        return ids, pitch, mel
+        return ids, pitch, mel.transpose(0, 2, 1)
 
     def compute_loss(self, bunch: List[np.ndarray]) \
             -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
@@ -70,6 +67,7 @@ class TrainingWrapper:
         Returns:
             loss and dictionaries.
         """
+        ## 1. Dynamic scheduler loss
         # [1 + S]
         logsnr, _ = self.model.scheduler()
         # [1 + S]
@@ -79,10 +77,18 @@ class TrainingWrapper:
             torch.clamp_min(1 - alphas_bar[-1], 1e-7)) + torch.log(
                 torch.clamp_min(alphas_bar[0], 1e-7))
 
-        # [B], [B, T], [B, T, mel]
+        # [B], [B, T], [B, mel, T]
         ids, pitch, mel = self.wrap(self.random_segment(bunch))
-        # [B, mel, T]
-        mel = mel.transpose(1, 2)
+        # B
+        bsize = ids.shape[0]
+        # [], range [1, B - 1]
+        start = np.random.randint(bsize - 1) + 1
+        # [B], for shuffling
+        indices = (np.arange(bsize) + start) % bsize
+        # [B, styles]
+        style = self.model.encoder(mel, ids)
+
+        ## 2. Cyclic noise estimation
         # [B], zero-based
         steps = torch.randint(
             self.config.model.steps, (mel.shape[0],), device=mel.device)
@@ -91,9 +97,30 @@ class TrainingWrapper:
         # [B, mel, T]
         base = mean + torch.randn_like(mean) * std[:, None, None]
         # [B, mel, T]
-        denoised = self.model.denoise(base, mel, steps)
+        converted = self.model.denoise(base, mel, style[indices], steps)
+
+
+
         # []
         noise_estim = (denoised - mel).square().mean()
+
+        ## 3. Cycle consistency
+
+
+        ## 3. Average pitch estimation and speaker classification
+        # [B, domains], [B]
+        logits, avgpit = self.disc(denoised)
+        # []
+        spkclf = F.cross_entropy(logits, ids)
+
+        ## 4. Cycle consistency
+
+        ## 5. ASR-guiding phonetic structure preservation
+
+        ## 6. Style reconstruction
+
+        ## 7. Norm consistency
+
         # total loss
-        loss = schedule_loss + noise_estim
+        loss = schedule_loss + noise_estim + spkclf
         return loss, {'sched': schedule_loss.item(), 'estim': noise_estim.item()}
